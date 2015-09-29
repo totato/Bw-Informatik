da82d379a5d73ae3b119a4025916fd7a7f2da2aa
Vika <Vika@GLaDOS.fritz.box>
2015-09-29 18:56:31 +0200

    ein Anfang

--- Ameisenfutter/src/ameisenfutter/Feld.java
+++ Ameisenfutter/src/ameisenfutter/Feld.java
@@ -9,15 +9,6 @@ package ameisenfutter;
  *
  * @author Vika
  */
-public class Simulation {
-
-    public Simulation() {
-        feldErzeugen(500, 5);
-    }
-
-    private int[] feldErzeugen(int größe, int futterquellen) {
-        int[][] feld = new int[größe][größe];
-        return null;
-
-    }
+public class Feld {
+    
 }
