a51492ada85db16f1d41c1186dee97e7f65e3911
Marc <Marc@MarcPC.fritz.box>
2015-09-29 19:20:39 +0200

    Konstruktor der Klasse Feld erstellt.

--- Ameisenfutter/src/ameisenfutter/Feld.java
+++ Ameisenfutter/src/ameisenfutter/Feld.java
@@ -10,5 +10,18 @@ package ameisenfutter;
  * @author Vika
  */
 public class Feld {
-    
+
+    private int X; //X-Koordinate des Feldes innerhalb des zweidimensionalen Arrays
+    private int Y; //Y-Koordinate des Feldes innerhalb des zweidimensionalen Arrays
+    private boolean isDuftstoffVerspruet; // gibt an, ob ein Feld mit Duftstoffen versehen wurde
+    private int futterportion; // Anzahl der auf dem Feld liegenden Portion Futter
+
+    //Konstruktor der Klasse Feld
+    public Feld(int X, int Y, boolean isDuftstoffVerspruet, int futterportion) {
+        this.X = X;
+        this.Y = Y;
+        this.futterportion = futterportion;
+        this.isDuftstoffVerspruet = isDuftstoffVerspruet;
+    }
+
 }
